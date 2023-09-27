f3b70ff50702362f039c34daa1bb1ae93a2c1cd6
Zac R <zac.ronayne@savoye.com>
2023-09-26 22:49:22 -0400

    Board can be populated with words

--- src/game/BingoGame.tsx
+++ src/game/BingoGame.tsx
@@ -2,15 +2,23 @@ import Grid from "./Grid";
 
 const BingoGame = () => {
   document.title = "BINGO";
-  return <>
-    <div style={{padding: "20px", fontSize: "80px", border: "2px solid #DDDDDD", background: "#222222", color: "#DDDDDD"}}>
+  return <div style={{textAlign: "center", margin: "20px"}}>
+    <div
+      style={{
+        padding: "20px", fontSize: "80px", border: "2px solid #DDDDDD", background: "#222222", color: "#DDDDDD",
+        width: "600px"
+      }}
+    >
       BINGO
     </div>
     <Grid
       width={5}
       height={5}
+      words={[
+      ]}
+      freeSpace="(FREE SPACE)"
     />
-  </>;
+  </div>;
 }
 
 export default BingoGame;
--- src/game/Grid.tsx
+++ src/game/Grid.tsx
@@ -9,17 +9,21 @@ export interface BoardSpot{
 interface GridProps{
   width: number;
   height: number;
+  words: string[];
+  freeSpace: string;
 }
 
 const Grid: FC<GridProps> = (props) => {
 
+  const GRID_SIZE = 110;
+  const TEXT_SCALER = 9000;
   const [board, setBoard] = useState<BoardSpot[][]>([]);
 
   useEffect(() => {
-    initBoard();
+    randomWords(initBoard(board));
   }, [props]);
 
-  const initBoard = () => {
+  const initBoard = (board: BoardSpot[][]) => {
     let newBoard: BoardSpot[][] = [];
     for(let y = 0; y < props.height; y++){
       newBoard.push([]);
@@ -28,6 +32,33 @@ const Grid: FC<GridProps> = (props) => {
       }
     }
     setBoard(newBoard);
+    return newBoard;
+  }
+
+  const randomWords = (board: BoardSpot[][]) => {
+    let newBoard = [...board];
+    let remaining = [...props.words];
+    for(let x = 0; x < props.width; x++){
+      for(let y = 0; y < props.height; y++){
+        // Check for free space
+        if(Math.floor(props.width / 2) === x && Math.floor(props.height / 2) === y){
+          newBoard[x][y].text = props.freeSpace;
+          continue;
+        }
+
+        // If there are no more words, use blank
+        if(remaining.length <= 0){
+          newBoard[x][y].text = "";
+          continue;
+        }
+        // Pick a random word and remove it from the list
+        let i = Math.floor(Math.random() * remaining.length);
+        newBoard[x][y].text = remaining[i];
+        remaining.splice(i, 1);
+      }
+    }
+    setBoard(newBoard);
+    return newBoard;
   }
 
   const toggleClicked = (x: number, y: number) => {
@@ -36,29 +67,49 @@ const Grid: FC<GridProps> = (props) => {
     setBoard(newBoard);
   } 
 
+  const findFontSize = (s: string) => {
+    // TODO find a better way of doing this
+    return Math.sqrt(TEXT_SCALER / s.length) + "px";
+  }
+
+  //  TODO abstract out styling
   return <>
-    <div
-      style={{
-        fontSize: "29px", border: "2px solid #DDDDDD", background: "#222222", 
-        width: "100px", textAlign: "center", color: "#DDDDDD", margin: "30px", padding: "20px"
-      }}
-      onClick={initBoard}
-    >
-      RESET
-    </div>
+    <Row>
+      <div
+        style={{
+          fontSize: "29px", border: "2px solid #DDDDDD", background: "#222222", 
+          width: "200px", textAlign: "center", color: "#DDDDDD", margin: "30px", padding: "20px"
+        }}
+        onClick={() => initBoard(board)}
+      >
+        RESET SPACES
+      </div>
+      <div
+        style={{
+          fontSize: "29px", border: "2px solid #DDDDDD", background: "#222222", 
+          width: "200px", textAlign: "center", color: "#DDDDDD", margin: "30px", padding: "20px"
+        }}
+        onClick={() => randomWords(board)}
+      >
+        NEW BOARD
+      </div>
+    </Row>
     {board.map((row, x) => <Row>
       {row.map((col, y) => <Col>
-        {/* TODO abstract out styling */}
         <div
           style={{
-            border: "2px solid #DDDDDD", padding: "10px",
-            background: col.filled ? "#DD0000" : "#222222", fontSize: "29px", color: "#DDDDDD"
+            border: "2px solid #DDDDDD", padding: "20px",
+            background: col.filled ? "#BB0000" : "#222222", fontSize: "29px", color: "#DDDDDD",
+            userSelect: "none",
+            width: GRID_SIZE + "px", height: GRID_SIZE + "px"
           }}
           onClick={() => {
             toggleClicked(x, y);
           }}
         >
-          a
+          <div style={{fontSize: findFontSize(col.text)}}>
+            {col.text}
+          </div>
         </div>
       </Col>)}
     </Row>)
