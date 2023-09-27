1806a735369680142cd591d4786ef0ab8a7e0fae
Zac R <zac.ronayne@savoye.com>
2023-09-26 21:56:04 -0400

    Finally got ts to stop being a psychopath

--- src/game/Grid.tsx
+++ src/game/Grid.tsx
@@ -1,10 +1,16 @@
 import { Row, Col } from "antd";
+import { useState } from "react";
 
 const Grid = () => {
+
+  const [board, setBoard] = useState<any[][]>([["a"]]);
+
   return <>
+    <div onClick={() => setBoard([["b"]])}> a</div>
     <Row>
       <Col>BINGO</Col>
     </Row>
+    {board[0]}
   </>;
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,4 @@ import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-// No idea why type script complains if this dosn't say as something
-root.render(<App /> as React.ReactNode);
+root.render(<App />);
