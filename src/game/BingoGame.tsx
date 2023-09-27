bd9847c7c718729dabc2b76ba1acfb9e98aaffeb
Zac R <zac.ronayne@savoye.com>
2023-09-26 20:17:12 -0400

    Adding antd and fixing type script being weird

--- src/game/BingoGame.tsx
+++ src/game/BingoGame.tsx
@@ -1,6 +1,10 @@
+import Grid from "./Grid";
+
 const BingoGame = () => {
   document.title = "BINGO";
-  return <>BINGOOOOOOOO</>;
+  return <>
+    <Grid/>
+  </>;
 }
 
 export default BingoGame;
---
+++ src/game/Grid.tsx
@@ -0,0 +1,11 @@
+import { Row, Col } from "antd";
+
+const Grid = () => {
+  return <>
+    <Row>
+      <Col>BINGO</Col>
+    </Row>
+  </>;
+};
+
+export default Grid;
